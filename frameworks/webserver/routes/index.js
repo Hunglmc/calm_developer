246244e33e6557c09e56a3a666f887cd11c47882
hunglm <hunglm@calm.com>
2023-05-12 23:47:10 +0700

    sửa lỗi code

--- frameworks/webserver/routes/index.js
+++ frameworks/webserver/routes/index.js
@@ -1,6 +1,7 @@
 import postRouter from './post';
 import userRouter from './user';
 import authRouter from './auth';
+import categoryRouter from './category';
 
 export default function routes(app, express, redisClient) {
     app.use('/api/v1/posts', postRouter(express, redisClient));
